bc2746c268ad54476adab7e4ccfb79b0480941da
Андрей <Barbus1966@gmail.com>
2024-12-08 18:38:08 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+#Переменные
+Home_work = 12
+hours = 1.5
+course = 'Python'
+time_for_one_task = 1.5/12
+print('Курс:'+ course+',','всего задач:',str(Home_work)+',','затрачено часов:',str(hours)+',','среднее время выполнения:',time_for_one_task,'часа.')
